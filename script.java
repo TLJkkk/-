f3149c2b5bb621b2a18fdbcf38c1dd926439f898
TLJkkk <118377793+TLJkkk@users.noreply.github.com>
2024-04-24 00:27:13 +0800

    Add files via upload

---
+++ script.java
@@ -0,0 +1,4 @@
+document.getElementById('couponButton').addEventListener('click', function() {
+    var couponImage = document.getElementById('couponImage');
+    couponImage.style.display = 'block'; // 显示优惠券图片
+});
